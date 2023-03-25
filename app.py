ba4bca92f1566336338fe8b8ac99d5370305ab77
Muniu Mark <markpetermuniu@gmail.com>
2023-03-25 06:24:48 +0300

    CHORE: Moved app.py to flask_server folder Due to backend organisation all flask essential files are packaged into flask_server

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-app.secret_key = "2e8d325bb6efd3c9"
-
-@app.route("/main")
-def index():
-    return render_template("index.html")
-    
